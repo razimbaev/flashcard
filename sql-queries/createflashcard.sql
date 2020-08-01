5e7f2fa4d1d7f901330b60ed93cf6fa6fac62481
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-08-01 16:18:27 -0400

    Move Tag name into separate table and add grant user privilege queries

--- sql-queries/createflashcard.sql
+++ sql-queries/createflashcard.sql
@@ -1,8 +1,9 @@
-DROP TABLE Tags;
+DROP TABLE Flashcard_Tag
+DROP TABLE Tag;
 DROP TABLE CardBackContent;
-DROP TABLE FlashCard;
+DROP TABLE Flashcard;
 
-CREATE TABLE FlashCard(
+CREATE TABLE Flashcard(
 	flashcard_id SERIAL PRIMARY KEY NOT NULL,
 	card_front VARCHAR(200) NOT NULL,
 	card_back VARCHAR(4000) NOT NULL
@@ -18,11 +19,28 @@ CREATE TABLE CardBackContent(
 			REFERENCES FlashCard(flashcard_id)
 );
 
-CREATE TABLE Tags(
+CREATE TABLE Tag(
+	tag_name VARCHAR(100) PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE Flashcard_Tag(
 	flashcard_id INT,
 	tag_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY(flashcard_id, tag_name),
-	CONSTRAINT flashId
+	CONSTRAINT flashcard_id_FK
 		FOREIGN KEY(flashcard_id)
-			REFERENCES FlashCard(flashcard_id)
+			REFERENCES FlashCard(flashcard_id),
+	CONSTRAINT tag_name_FK
+		FOREIGN KEY(tag_name)
+			REFERENCES Tag(tag_name)
 );
+
+GRANT ALL ON SEQUENCE public.flashcard_flashcard_id_seq TO flashcard_user;
+
+GRANT ALL ON TABLE public.cardbackcontent TO flashcard_user;
+
+GRANT ALL ON TABLE public.flashcard TO flashcard_user;
+
+GRANT ALL ON TABLE public.tag TO flashcard_user;
+
+GRANT ALL ON TABLE public.flashcard_tag TO flashcard_user;
